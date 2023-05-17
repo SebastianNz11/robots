cb66fa3db1192f760e6fa971bff4ce28c134102b
Sebastián Nuñez <sebas11np@gmail.com>
2023-05-17 12:18:04 -0600

    Agregando más secciones

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,27 @@
-var typed = new Typed('#element', {
-    strings: ['<i>Hola!</i>', 'bienvenido a', 'Blue Wolf Robotics'],
+const typed = new Typed('#element', {
+    strings: ['¡Hola!', 'bienvenido a', 'Blue Wolf Robotics'],
     typeSpeed: 70,
   });
+
+  window.sr = ScrollReveal();
+  sr.reveal('#headline', {
+    duration: 3000,
+    origin: 'top',
+    distance:'55px'
+  })
+
+  window.sr = ScrollReveal();
+  sr.reveal('.catalogo', {
+    duration: 3000,
+    origin: 'right',
+    distance:'55px'
+  })
+
+  window.sr = ScrollReveal();
+  sr.reveal('#headlineB', {
+    duration: 3000,
+    origin: 'bottom',
+    distance:'55px'
+  })
+
+ 
